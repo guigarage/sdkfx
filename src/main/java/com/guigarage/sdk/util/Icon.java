0d15a067456518f9ac033d6db6fcfae6cafba6a1
Brian Schlining <bschlining@gmail.com>
2015-07-19 15:42:56 -0700

    Refactored Icon to be more generically usable

--- src/main/java/com/guigarage/sdk/util/Icon.java
+++ src/main/java/com/guigarage/sdk/util/Icon.java
@@ -1,23 +1,11 @@
 package com.guigarage.sdk.util;
 
 /**
- * Created by hendrikebbers on 12.03.15.
+ * Icons in sdkfx are implemented via fontawesome. This interface just returns the text character
+ * @author Brian Schlining
+ * @since 2015-07-19T15:32:00
  */
-public enum Icon {
-    VOLUMNE_UP("\uf028"), VOLUMNE_DOWN("\uf027"), ANGLE_LEFT("\uf104"), ANGLE_RIGHT("\uf105"), NAV("\uf0c9"), COGS("\uf085"), MAIL("\uf003"), CALENDAR("\uf073"), PHONE("\uf095");
+public interface Icon {
 
-    private String text;
-
-    Icon(String text) {
-        this.text = text;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    @Override
-    public String toString() {
-        return getText();
-    }
+    String getText();
 }
